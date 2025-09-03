9b3ce3c839cf3bfc0710d9c4fd49a3fa5cb3a57b
uvw222 <yuval2wil@gmail.com>
2025-09-03 22:46:30 +0300

    Fix Next.js directive order in chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,8 @@
-export const dynamic = "force-dynamic";
 
 "use client";
+
+export const dynamic = "force-dynamic";
+
 
 import { useRef, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
