2417c0609c3753fae4f12395fbb988fe8ca3b9ae
uvw222 <yuval2wil@gmail.com>
2025-09-07 16:06:31 +0300

    Use pages/api/socket.ts (Node) for Socket.IO; remove app/api/socket route; wire global._io for chat

---
+++ pages/api/socket.ts
@@ -0,0 +1,45 @@
+﻿import { Server as IOServer } from "socket.io";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Server as HTTPServer } from "http";
+import type { Socket as NetSocket } from "net";
+
+type NextApiResponseWithSocket = NextApiResponse & {
+  socket: NetSocket & { server: HTTPServer & { io?: IOServer } };
+};
+
+export default function SocketHandler(
+  _req: NextApiRequest,
+  res: NextApiResponseWithSocket
+) {
+  if (!res.socket.server.io) {
+    console.log(" Initializing Socket.io server @ /api/socket");
+    const io = new IOServer(res.socket.server as unknown as HTTPServer, {
+      path: "/api/socket",
+      addTrailingSlash: false,
+      cors: { origin: "*", methods: ["GET", "POST"] },
+    });
+
+    res.socket.server.io = io;
+    (global as any)._io = io;
+
+    io.on("connection", (socket) => {
+      console.log(" Socket connected:", socket.id);
+
+      socket.on("join", (email: string) => {
+        if (email) {
+          socket.join(email);
+          console.log(` ${email} joined room`);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log(" Socket disconnected:", socket.id);
+      });
+    });
+  } else {
+    (global as any)._io = res.socket.server.io;
+    console.log(" Reusing existing Socket.io server");
+  }
+
+  res.end();
+}
